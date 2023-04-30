60e295bf4d894a6a09ad3204c5a109e32a46cde1
Julia <zamusinskaaulia@gmail.com>
2023-04-30 18:08:32 +0300

    Addet Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход два числа 
+// и проверяет, кратно ли оно одновременно 7 и 23
+// 14 -> Нет
+// 161 -> да
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool result = Multiplicity(number);
+Console.WriteLine(result ? "Да" : "Нет");
+
+bool Multiplicity(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;                 
+}
