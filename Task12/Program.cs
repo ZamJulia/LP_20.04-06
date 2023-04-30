e511ac9eb88c3366a7ec8e1ba215c02c47a2f8a5
Julia <zamusinskaaulia@gmail.com>
2023-04-30 18:08:00 +0300

    Addet Task12

---
+++ Task12/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая будет принимать на вход два числа
+// и выводить , является ли первое число кратным второму,
+// то программа выводит остаток от деления
+// 34,5 -> не кратно , остаток 4
+// 16,4 -> кратно 
+
+// Console.Write ("Первое число");
+// int number1 =Convert.ToInt32(Console.ReadLine());
+// Console.Write("Второе число");
+// int number2 = Convert.ToInt32(Console.ReadLine()); 
+
+// int remainder = number1 % number2;
+
+// if( remainder == 0)
+// {
+//    Console.WriteLine("Кратно");                   
+// }
+// else
+// {
+//    Console.WriteLine($"Не кратно, остаток = {remainder}");                   
+// }
+
+Console.Write("Первое число =");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Второе число =");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int remainder = Remainder(number1, number2);
+string result = remainder != 0 ? $"Не кратно, остаток = {remainder}" : "кратно";
+Console.WriteLine(result);
+
+int Remainder(int num1, int num2)
+{
+    return num1 % num2;
+}
