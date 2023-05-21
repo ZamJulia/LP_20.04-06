ef0184ff7f4413ddb5ff8e197e8845bd160b3ec1
Julia <zamusinskaaulia@gmail.com>
2023-05-21 20:45:24 +0300

    Добавить папку Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = CountNumbers(number);
+
+Console.WriteLine($"Кол-во цифр в числе {number} =  {count}");
+
+
+int CountNumbers (int num)
+{
+    int i = 0;
+    while (num != 0) 
+    {
+        num = num / 10;
+        i++;
+    }
+    return i;
+}
